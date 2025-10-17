aab723a97679af5d282d63e2e45a05d978293c73
meipowong5 <meipowong5@gapps.cityu.edu.hk>
2025-10-17 18:28:40 +0800

    Exercise4Final

--- code.js
+++ code.js
@@ -16,7 +16,7 @@ button.addEventListener("click", myOtherfunction);
 
 function myOtherfunction() {
     alert("Writing, drawing, dancing ♡〜٩( ˃▿˂ )۶〜♡");
-}
+}*/
 
 var button = document.querySelector("#bless");
 
@@ -24,41 +24,6 @@ button.addEventListener("click", myOther2function);
 
 function myOther2function() {
     alert("Have a nice day!XD");
-}*/
-
-
-var my1stnode = document.createElement("div");
-my1stnode.id = "work1_intro";
-my1stnode.innerHTML = "My drawing on two girls.";
-my1stnode.style.color = "blue";
-
-my1stnode.addEventListener("click", welcomeToWork1);
-document.querySelector("#grid-item1").appendChild(my1stnode);
-
-function welcomeToWork1() {
-    my1stnode.innerHTML = "( ദ്ദി ˙ᗜ˙ )";
 }
 
-var mynode = document.createElement("div");
-mynode.id = "work2_intro";
-mynode.innerHTML = "My drawing on Magic Miku";
-mynode.style.color = "blue";
 
-mynode.addEventListener("click", welcomeToWork2);
-document.querySelector("#grid-item2").appendChild(mynode);
-
-function welcomeToWork2() {
-    mynode.innerHTML = "( ദ്ദി ˙ᗜ˙ )";
-}
-
-var my3rdnode = document.createElement("div");
-my3rdnode.id = "work3_intro";
-my3rdnode.innerHTML = "My black and white drawing";
-my3rdnode.style.color = "blue";
-
-my3rdnode.addEventListener("click", welcomeToWork3);
-document.querySelector("#grid-item3").appendChild(my3rdnode);
-
-function welcomeToWork3() {
-    my3rdnode.innerHTML = "( ദ്ദി ˙ᗜ˙ )";
-}
